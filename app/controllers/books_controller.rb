d6db76eb98e7cb4fc3846232924f3b77f9230e09
miyoooon <133766145+miyoooon@users.noreply.github.com>
2024-02-15 10:18:09 +0000

    [Finish] users/show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,10 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to user_path(@book.id)
   end
 
   def index
@@ -18,6 +22,12 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
 
   def show
    @user = User.find(params[:id])
+   @book = Book.new
+   @books = @user.books.all
   end
 
   def edit
