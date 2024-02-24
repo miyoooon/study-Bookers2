c622b49edc4483cf15dea6f01e40b1051b764430
miyoooon <133766145+miyoooon@users.noreply.github.com>
2024-02-24 07:53:59 +0000

    [Finish] books/show, books/edit, books/update, books/destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,23 +6,33 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to user_path(@book.id)
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
+    @newbook = Book.new
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
